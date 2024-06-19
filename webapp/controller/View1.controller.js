dc913fe1df1aebea865e91f8005666b61cb9c7f2
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-06-19 18:35:56 +0200

    Custom 'fold' Animation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -13,14 +13,14 @@ sap.ui.define([
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-core');
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-effect')
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-core');
-                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-fade');
+                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-fold');
                 
                         sap.m.NavContainer.transitions["custom"] = {
             
                     to: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("fade");
-                            oToPage.$().toggle("fade");
+                            oFromPage.$().toggle("fold");
+                            oToPage.$().toggle("fold");
                             fCallback();
                         },600);
                         
@@ -28,8 +28,8 @@ sap.ui.define([
             
                     back: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("fade");
-                            oToPage.$().toggle("fade");
+                            oFromPage.$().toggle("fold");
+                            oToPage.$().toggle("fold");
                             fCallback();
                         },600);
                     }
