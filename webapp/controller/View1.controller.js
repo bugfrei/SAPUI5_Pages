1c1714f6ee6aa42317961d290346694a880990fa
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-06-19 18:32:10 +0200

    Custom 'drop' Animation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -13,14 +13,14 @@ sap.ui.define([
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-core');
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-effect')
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-core');
-                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-clip');
+                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-drop');
                 
                         sap.m.NavContainer.transitions["custom"] = {
             
                     to: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("clip");
-                            oToPage.$().toggle("clip");
+                            oFromPage.$().toggle("drop");
+                            oToPage.$().toggle("drop");
                             fCallback();
                         },600);
                         
@@ -28,8 +28,8 @@ sap.ui.define([
             
                     back: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("clip");
-                            oToPage.$().toggle("clip");
+                            oFromPage.$().toggle("drop");
+                            oToPage.$().toggle("drop");
                             fCallback();
                         },600);
                     }
