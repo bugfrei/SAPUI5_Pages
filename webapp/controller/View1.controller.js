1d5e5c80a832d113d74e7ed191f322c2e0c78bb2
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-06-19 18:41:11 +0200

    Custom 'shake' Animation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -13,14 +13,14 @@ sap.ui.define([
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-core');
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-ui-effect')
                 jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-core');
-                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-scale');
+                jQuery.sap.require('sap.ui.thirdparty.jqueryui.jquery-effects-shake');
                 
                         sap.m.NavContainer.transitions["custom"] = {
             
                     to: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("scale");
-                            oToPage.$().toggle("scale");
+                            oFromPage.$().toggle("shake");
+                            oToPage.$().toggle("shake");
                             fCallback();
                         },600);
                         
@@ -28,8 +28,8 @@ sap.ui.define([
             
                     back: function(oFromPage, oToPage, fCallback) {
                         window.setTimeout(function(){
-                            oFromPage.$().toggle("scale");
-                            oToPage.$().toggle("scale");
+                            oFromPage.$().toggle("shake");
+                            oToPage.$().toggle("shake");
                             fCallback();
                         },600);
                     }
